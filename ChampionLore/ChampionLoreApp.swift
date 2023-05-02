695e34dfd173f5ec689bb36d885965a305244651
David Mclean <47851457+davupls@users.noreply.github.com>
2023-05-02 05:24:15 -0500

    Initial Commit

---
+++ ChampionLore/ChampionLoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionLoreApp.swift
+//  ChampionLore
+//
+//  Created by David Mclean on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ChampionLoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
